e51cd8074e2f24a2fae316a9f60716126ab03869
axlotyl-byte <altheagavilo82@gmail.com>
2025-08-11 21:52:26 +0300

    Add Part 2: Error handling in Future

---
+++ lib/part1_future.dart
@@ -0,0 +1,2 @@
+* A function that uses Future.delayed to return a message after 2 seconds.
+* A sync main() function that calls it with await and prints the result.
---
+++ lib/part2_error_handling.dart
@@ -0,0 +1,2 @@
+* A function that stimulates an error inside a Future.delayed.
+* A try/catch block in main() that catches and prints an error message.
